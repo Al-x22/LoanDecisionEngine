b99c84c076dfb007fbe013d06c5e2e64dffb38ef
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-23 14:12:29 +0200

    DATABASE; Hardcoded users created in Repository folder

---
+++ src/main/java/com/example/loandecisionengine/repository/UserExamples.java
@@ -0,0 +1,23 @@
+package com.example.loandecisionengine.repository;
+
+import com.example.loandecisionengine.entity.User;
+
+public class UserExamples {
+
+    User example1 = new User(
+            49002010965L,
+            0
+    );
+    User example2 = new User(
+            49002010976L,
+            1
+    );
+    User example3 = new User(
+            49002010987L,
+            2
+    );
+    User example4 = new User(
+            49002010998L,
+            3
+    );
+}
