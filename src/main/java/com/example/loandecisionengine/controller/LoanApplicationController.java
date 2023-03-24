b178e610a68f449ebb6c8fa4fc196505b3f60365
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-24 02:13:36 +0200

    HTML; Add Form for POST execution

--- src/main/java/com/example/loandecisionengine/controller/LoanApplicationController.java
+++ src/main/java/com/example/loandecisionengine/controller/LoanApplicationController.java
@@ -1,9 +1,14 @@
 package com.example.loandecisionengine.controller;
 
+
 import com.example.loandecisionengine.entity.LoanApplication;
+import com.example.loandecisionengine.entity.LoanApplicationResult;
+import com.example.loandecisionengine.service.GenerateLoanApplicationResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoanApplicationController {
@@ -11,6 +16,13 @@ public class LoanApplicationController {
     @GetMapping("/loan")
     public String showForm(Model model) {
         model.addAttribute("loanApplication", new LoanApplication());
+        return "loanApplicationForm";
+    }
+    @PostMapping("/loan")
+    public String processForm(@ModelAttribute LoanApplication loanApplication, Model model) {
+        LoanApplicationResult loanApplicationResult =
+                new GenerateLoanApplicationResult().generateLoanApplicationResult(loanApplication);
+        model.addAttribute("loanApplicationResult", loanApplicationResult);
         return "loanApplicationForm";
     }
 }
