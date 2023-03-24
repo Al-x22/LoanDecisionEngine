e72c9ba3d113ddbea601f6b3c1f415ee4f2838b0
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-24 02:12:44 +0200

    CLASS; add attributes to LoanAppResult

--- src/main/java/com/example/loandecisionengine/entity/LoanApplicationResult.java
+++ src/main/java/com/example/loandecisionengine/entity/LoanApplicationResult.java
@@ -2,28 +2,56 @@ package com.example.loandecisionengine.entity;
 
 public class LoanApplicationResult {
 
-    private String decision;
-    private int amount;
+    private boolean decision;
+    private int possibleAmountInOriginalMonths;
+    private int finalAmount;
+    private int finalMonths;
+    private LoanApplication applicationForm;
 
-    public LoanApplicationResult(String decision, int amount) {
+    public LoanApplicationResult(boolean decision) {
         this.decision = decision;
-        this.amount = amount;
     }
-
-    public String getDecision() {
+    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths) {
+        this.decision = decision;
+    }
+    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths, int finalAmount, int finalMonths, LoanApplication applicationForm) {
+        this.decision = decision;
+        this.possibleAmountInOriginalMonths = possibleAmountInOriginalMonths;
+        this.finalAmount = finalAmount;
+        this.finalMonths = finalMonths;
+        this.applicationForm = applicationForm;
+    }
+    public boolean getDecision() {
         return decision;
     }
-
-    public void setDecision(String decision) {
+    public void setDecision(boolean decision) {
         this.decision = decision;
     }
-
-    public int getAmount() {
-        return amount;
+    public int getPossibleAmountInOriginalMonths() {
+        return possibleAmountInOriginalMonths;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    public void setPossibleAmountInOriginalMonths(int possibleAmountInOriginalMonths) {
+        this.possibleAmountInOriginalMonths = possibleAmountInOriginalMonths;
+    }
+    public int getFinalAmount() {
+        return finalAmount;
+    }
+    public void setFinalAmount(int finalAmount) {
+        this.finalAmount = finalAmount;
+    }
+    public int getFinalMonths() {
+        return finalMonths;
+    }
+
+    public void setFinalMonths(int finalMonths) {
+        this.finalMonths = finalMonths;
+    }
+    public LoanApplication getApplicationForm() {
+        return applicationForm;
+    }
+    public void setApplicationForm(LoanApplication applicationForm) {
+        this.applicationForm = applicationForm;
     }
 
 }
