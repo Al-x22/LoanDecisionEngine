176d2a5539336ff54fd5306cbdb6a72f5a881785
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-24 12:49:15 +0200

    Service; Add Motive Message and Refactor

--- src/main/java/com/example/loandecisionengine/entity/LoanApplicationResult.java
+++ src/main/java/com/example/loandecisionengine/entity/LoanApplicationResult.java
@@ -5,28 +5,39 @@ public class LoanApplicationResult {
     private boolean decision;
     private int possibleAmountInOriginalMonths;
     private int finalAmount;
-    private int finalMonths;
+    private int finalLoanPeriod = 0;
+    private String motiveMsg;
     private LoanApplication applicationForm;
 
-    public LoanApplicationResult(boolean decision) {
+    public LoanApplicationResult(boolean decision, String motiveMsg) {
         this.decision = decision;
+        this.motiveMsg = motiveMsg;
     }
-    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths) {
+
+    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths, String motiveMsg) {
         this.decision = decision;
+        this.possibleAmountInOriginalMonths = possibleAmountInOriginalMonths;
+        this.motiveMsg = motiveMsg;
     }
-    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths, int finalAmount, int finalMonths, LoanApplication applicationForm) {
+
+    public LoanApplicationResult(boolean decision, int possibleAmountInOriginalMonths,
+                                 int finalAmount, int finalLoanPeriod, String motiveMsg, LoanApplication applicationForm) {
         this.decision = decision;
         this.possibleAmountInOriginalMonths = possibleAmountInOriginalMonths;
         this.finalAmount = finalAmount;
-        this.finalMonths = finalMonths;
+        this.finalLoanPeriod = finalLoanPeriod;
+        this.motiveMsg = motiveMsg;
         this.applicationForm = applicationForm;
     }
+
     public boolean getDecision() {
         return decision;
     }
+
     public void setDecision(boolean decision) {
         this.decision = decision;
     }
+
     public int getPossibleAmountInOriginalMonths() {
         return possibleAmountInOriginalMonths;
     }
@@ -34,22 +45,31 @@ public class LoanApplicationResult {
     public void setPossibleAmountInOriginalMonths(int possibleAmountInOriginalMonths) {
         this.possibleAmountInOriginalMonths = possibleAmountInOriginalMonths;
     }
+
     public int getFinalAmount() {
         return finalAmount;
     }
+
     public void setFinalAmount(int finalAmount) {
         this.finalAmount = finalAmount;
     }
-    public int getFinalMonths() {
-        return finalMonths;
+
+    public int getFinalLoanPeriod() {
+        return finalLoanPeriod;
     }
 
-    public void setFinalMonths(int finalMonths) {
-        this.finalMonths = finalMonths;
+    public void setFinalLoanPeriod(int finalLoanPeriod) {
+        this.finalLoanPeriod = finalLoanPeriod;
     }
+
+    public String getMotiveMsg() { return motiveMsg;}
+
+    public void setMotiveMsg(String motiveMsg) {this.motiveMsg = motiveMsg;}
+
     public LoanApplication getApplicationForm() {
         return applicationForm;
     }
+
     public void setApplicationForm(LoanApplication applicationForm) {
         this.applicationForm = applicationForm;
     }
