50b02d481e279237e8d6036bf3d56ca237a7f1d7
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-24 02:10:20 +0200

    REPOSITORY; Add getUserByPersonalCode

--- src/main/java/com/example/loandecisionengine/repository/UserExamples.java
+++ src/main/java/com/example/loandecisionengine/repository/UserExamples.java
@@ -2,7 +2,13 @@ package com.example.loandecisionengine.repository;
 
 import com.example.loandecisionengine.entity.User;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class UserExamples {
+
+    public UserExamples() {
+    }
 
     User example1 = new User(
             49002010965L,
@@ -20,4 +26,16 @@ public class UserExamples {
             49002010998L,
             3
     );
+
+    List<User> usersExamples = Arrays.asList(example1, example2, example3, example4);
+
+    public User getUserByPersonalCode(long personalCode) {
+
+        for (User user : usersExamples){
+            if (user.getPersonalCode() == personalCode){
+                return user;
+            }
+        }
+        return null;
+    }
 }
