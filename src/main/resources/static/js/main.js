845dd4b7720069c786d1fe11c7877463f7c308e8
Alejandro Ballesteros Perez <aballe@ttu.ee>
2023-03-25 01:55:10 +0200

    JS; Dynamic slide

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("loanAmount").value = 2000;
+document.getElementById("loanPeriod").value = 12;
+
+function getValueFromSliderLoanAmount(val) {
+    document.getElementById("loanAmount").value = val;
+}
+function getValueFromSliderLoanPeriod(val) {
+    document.getElementById("loanPeriod").value = val;
+}
